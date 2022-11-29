a3af2199332b4bc73fc18959bb6ddf305fbdcbd3
LeeJiHyeon <rachel2148072@gmail.com>
2022-11-29 23:12:13 +0900

    egoing_react : MUI
    - 올인원 디자인 UI 모음
    - 설치 필요 : npm install @mui/material @emotion/react @emotion/styled

--- react_egoing/1122/styled/src/Article.js
+++ react_egoing/1122/styled/src/Article.js
@@ -1,25 +1,19 @@
 import React from "react";
-import styled from "styled-components";
-
-const Button1 = props => {
-  console.log(props);
-  return (
-    <button style={{ color: "green", fontSize: "2rem" }}>{props.label}</button>
-  );
-};
-const Button2 = styled.button`
-  color: green;
-  font-size: 2rem;
-`;
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
 export const Article = () => {
   return (
     <article>
       <h2>Welcome</h2>
       Hello, React!
-      <hr />
-      <Button1 label={"Create"}>test</Button1>
-      <Button2>update</Button2>
+      <p>
+        <ButtonGroup variant="contained">
+          <Button>Create</Button>
+          <Button>Update</Button>
+        </ButtonGroup>
+        <Button variant="outlined">Delete</Button>
+      </p>
     </article>
   );
 };
