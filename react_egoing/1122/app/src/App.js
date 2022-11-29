4b6ef2add08b0ebf2265d658df373ece6a3dfea0
LeeJiHyeon <rachel2148072@gmail.com>
2022-11-30 01:10:21 +0900

    egoing_react : 3) 복습

---
+++ react_egoing/1122/app/src/App.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+
+const Header = ({ title, onChangeMode }) => {
+  return (
+    <header>
+      <h1>
+        <a
+          href="index.html"
+          onClick={e => {
+            e.preventDefault();
+            onChangeMode("WELCOME");
+          }}>
+          {title}
+        </a>
+      </h1>
+    </header>
+  );
+};
+const Nav = ({ topics, onChangeMode }) => {
+  const liTag = topics.map(topic => (
+    <li key={topic.id}>
+      <a
+        href={`/read/${topic.id}`}
+        onClick={e => {
+          e.preventDefault();
+          onChangeMode("READ", topic.id);
+        }}>
+        {topic.title}
+      </a>
+    </li>
+  ));
+  return (
+    <nav>
+      <ul>{liTag}</ul>
+    </nav>
+  );
+};
+const Article = ({ title, body }) => {
+  return (
+    <article>
+      <h2>{title}</h2>
+      {body}
+    </article>
+  );
+};
+
+function App() {
+  const [mode, setMode] = useState("WELCOME");
+  const [id, setId] = useState(null);
+  const topics = [
+    { id: 1, title: "html", body: "html is ..." },
+    { id: 2, title: "css", body: "css is ..." },
+    { id: 3, title: "js", body: "js is ..." },
+  ];
+  const changeModeHadler = (mode, id) => {
+    setMode(mode);
+    if (id !== undefined) {
+      setId(id);
+    }
+  };
+  let content = null;
+  if (mode === "WELCOME") {
+    content = <Article title="Hello" body="Welcomem WEB!" />;
+  } else if (mode === "READ") {
+    const selected = topics.find(topic => topic.id === id);
+    content = <Article title={selected.title} body={selected.body} />;
+  }
+  return (
+    <div>
+      <Header title="웹" onChangeMode={changeModeHadler} />
+      <Nav topics={topics} onChangeMode={changeModeHadler} />
+      {content}
+    </div>
+  );
+}
+
+export default App;
